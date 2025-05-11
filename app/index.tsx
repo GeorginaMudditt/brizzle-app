475d14043865d19904bc65151db130e7a539b450
Bastien Duseaux <bastien.duseaux@gmail.com>
2025-05-11 15:32:24 +0200

    feat: add force default bg color for dark and light theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,89 +1,89 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import { theme } from "../theme";
-import { Link } from "expo-router";
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { theme } from '../theme';
+import { Link } from 'expo-router';
 
 // Welcome screen for the app
 // Offers user the options to get started with onboarding or to log in
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Image
-          source={require("../assets/brizzle-insta-square.png")}
-          style={styles.logoWithName}
-        />
-        <Text style={[styles.introText, styles.introTextHeader]}>
-          Bienvenue sur Brizzle
-        </Text>
-        <Text style={styles.introText}>
-          Une application de vocabulaire où vous pouvez progresser en anglais à
-          votre rythme et en vous amusant !
-        </Text>
-        <View style={styles.buttonContainer}>
-          <Link href="/login" asChild>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>Allons-y !</Text>
-            </TouchableOpacity>
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View>
+				<Image
+					source={require('../assets/brizzle-insta-square.png')}
+					style={styles.logoWithName}
+				/>
+				<Text style={[styles.introText, styles.introTextHeader]}>
+					Bienvenue sur Brizzle
+				</Text>
+				<Text style={styles.introText}>
+					Une application de vocabulaire où vous pouvez progresser en anglais à
+					votre rythme et en vous amusant !
+				</Text>
+				<View style={styles.buttonContainer}>
+					<Link href='/login' asChild>
+						<TouchableOpacity style={styles.button}>
+							<Text style={styles.buttonText}>Allons-y !</Text>
+						</TouchableOpacity>
+					</Link>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 50,
-  },
-  logoWithName: {
-    width: 180,
-    height: 180,
-    alignSelf: "center",
-    borderRadius: 100,
-    marginTop: 20,
-  },
-  introTextHeader: {
-    fontSize: 40,
-    color: theme.colorBlue,
-  },
-  introText: {
-    textAlign: "center",
-    color: theme.colorBlue,
-    fontSize: 30,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-  },
-  button: {
-    backgroundColor: theme.colorBlue,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    borderRadius: 5,
-    width: 250,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 20,
-  },
-  loginText: {
-    marginTop: 50,
-    textAlign: "center",
-    fontSize: 15,
-  },
-  loginTextLink: {
-    textAlign: "center",
-    textDecorationLine: "underline",
-    fontSize: 15,
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingVertical: 50,
+	},
+	logoWithName: {
+		width: 180,
+		height: 180,
+		alignSelf: 'center',
+		borderRadius: 100,
+		marginTop: 20,
+	},
+	introTextHeader: {
+		fontSize: 40,
+		color: theme.colorBlue,
+	},
+	introText: {
+		textAlign: 'center',
+		color: theme.colorBlue,
+		fontSize: 30,
+		paddingHorizontal: 30,
+		paddingVertical: 10,
+	},
+	button: {
+		backgroundColor: theme.colorBlue,
+		paddingHorizontal: 30,
+		paddingVertical: 10,
+		borderRadius: 5,
+		width: 250,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	buttonContainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1,
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 20,
+	},
+	loginText: {
+		marginTop: 50,
+		textAlign: 'center',
+		fontSize: 15,
+	},
+	loginTextLink: {
+		textAlign: 'center',
+		textDecorationLine: 'underline',
+		fontSize: 15,
+	},
 });
